9d5c073d5b70cef54db22cf9a929f141dfd84ac7
Alabê Duarte <aduarte@thoughtworks.com>
2014-07-18 13:17:30 -0300

    Change news ordering at page_spec.html

--- spec/newly/news_crawler_spec.rb
+++ spec/newly/news_crawler_spec.rb
@@ -31,24 +31,22 @@ describe Newly::NewsCrawler do
     end
 
     context "when news has content" do
+
+      it "should fetch high quality images" do
+        a_news = first_reader.fetch.first
+        a_news.image.should == "http://s.glbimg.com/en/ho/f/original/2012/09/29/exobeso.jpg"
+      end
+      it "should capitalize the title field" do
+        a_news = first_reader.fetch.first
+        a_news.title.should == "Fenomeno assustador"
+      end
+
       let(:a_news) { second_reader.fetch.first }
       it { a_news.url.should == 'http://esporte.uol.com.br/ultimas-noticias/reuters/2012/09/08/jackie-stewart-aconselha-hamilton-a-continuar-na-mclaren.htm' }
       it { a_news.title.should == 'Jackie Stewart aconselha Hamilton a continuar na McLaren' }
       it { a_news.subtitle.should == 'MONZA, 8 Set (Reuters) - Tricampeao de Formula 1, Jackie Stewart aconselhou Lewis Hamilton neste sabado a...' }
       it { a_news.feed.url == "http://noticias.uol.com.br/noticias" }
-    end
 
-    context "http://globo.com" do
-      context "validating 'http://www.globo.com' selector" do
-        it "should fetch high quality images" do
-          a_news = first_reader.fetch.first
-          a_news.image.should == "http://s.glbimg.com/en/ho/f/original/2012/09/29/exobeso.jpg"
-        end
-        it "should capitalize the title field" do
-          a_news = first_reader.fetch.first
-          a_news.title.should == "Fenomeno assustador"
-        end
-      end
     end
 
   end
